f9c0fa517c6276ac9b4894a690df2f3eba263a3e
mktinternacional <145680524+mktinternacional@users.noreply.github.com>
2023-10-10 07:11:20 -0300

    vad

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -439,3 +439,23 @@ function removeOverflowHiddenOnMobile() {
 
 // Chame a função no momento apropriado, por exemplo, após o carregamento da página
 removeOverflowHiddenOnMobile();
+
+
+function phoneMaskBrazil() {
+  var key = window.event.key;
+  var element = window.event.target;
+  var isAllowed = /\d|Backspace|Tab/;
+  if(!isAllowed.test(key)) window.event.preventDefault();
+  
+  var inputValue = element.value;
+  inputValue = inputValue.replace(/\D/g,'');
+  inputValue = inputValue.replace(/(^\d{2})(\d)/,'($1) $2');
+  inputValue = inputValue.replace(/(\d{4,5})(\d{4}$)/,'$1-$2');
+  
+  element.value = inputValue;
+}
+
+
+
+
+
